c73838ae7519d6b7f879691f77053b58c9463d65
MichalDurlak <durlak.m@wp.pl>
2020-12-25 16:43:46 +0100

    Create login layout + register layout Create navigate from main layout to register layout Create activity login btn -> check if login and password are empty then show toast

--- app/src/main/java/com/example/mypersonaltrainer/MainActivity.kt
+++ app/src/main/java/com/example/mypersonaltrainer/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.mypersonaltrainer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        /* Hide status bar */
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_main)
+
+        btn_LogIn.setOnClickListener(){
+
+            if (TextUtils.isEmpty(input_Login.text.toString()) || TextUtils.isEmpty(input_Password.text.toString())){
+                Toast.makeText(applicationContext, "First type your login and password", Toast.LENGTH_LONG).show()
+
+            } else {
+                Toast.makeText(applicationContext, "Checking login and password", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    btn_Register.setOnClickListener(){
+setContentView(R.layout.activity_register)
     }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/mypersonaltrainer/RegisterActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mypersonaltrainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.btn_Register
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+        btn_RegisterCancel.setOnClickListener(){
+            setContentView(R.layout.activity_main)
+        }
+
+    }
+}
