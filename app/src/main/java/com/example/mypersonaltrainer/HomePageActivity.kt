5eb1bddf39d9f40ea42d2b975bf3cf09d489e48e
MichalDurlak <durlak.m@wp.pl>
2021-01-01 21:55:44 +0100

    Added button to change theme Day(Light) -> Night(Dark)

--- app/src/main/java/com/example/mypersonaltrainer/HomePageActivity.kt
+++ app/src/main/java/com/example/mypersonaltrainer/HomePageActivity.kt
@@ -1,8 +1,12 @@
 package com.example.mypersonaltrainer
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_home_page.*
 
 class HomePageActivity : AppCompatActivity() {
@@ -10,7 +14,28 @@ class HomePageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
+        isUsingNightModeResources()
+        if (isUsingNightModeResources() == true){
+            btn_DayHomePage.setVisibility(View.VISIBLE)
+            btn_NightHomePage.setVisibility(View.GONE)
+        } else {
+            btn_NightHomePage.setVisibility(View.VISIBLE)
+            btn_DayHomePage.setVisibility(View.GONE)
+        }
 
+
+        btn_NightHomePage.setOnClickListener(){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            btn_NightHomePage.setVisibility(View.GONE)
+            btn_DayHomePage.setVisibility(View.VISIBLE)
+
+        }
+
+        btn_DayHomePage.setOnClickListener(){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            btn_NightHomePage.setVisibility(View.VISIBLE)
+            btn_DayHomePage.setVisibility(View.GONE)
+        }
 
 
         btn_Logout.setOnClickListener(){
@@ -20,5 +45,17 @@ class HomePageActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun isUsingNightModeResources(): Boolean {
+        return when (resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            Configuration.UI_MODE_NIGHT_NO -> false
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> false
+            else -> false
+        }
+    }
+
+
 
 }
---
+++ app/src/main/java/com/example/mypersonaltrainer/SidebarHomePage.kt
@@ -0,0 +1,11 @@
+package com.example.mypersonaltrainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SidebarHomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sidebar_home_page)
+    }
+}
